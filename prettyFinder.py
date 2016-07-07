672845d34d3908a289d4da928953cbea48a9b382
kiikura <keniikura@gmail.com>
2016-07-07 09:49:42 +0000

    add pretty Finder
    local file will be evaluated by prettiness

---
+++ prettyFinder.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import json
+
+from clarifai.client import ClarifaiApi
+
+
+def tag_images_in_directory(path, api):
+  images = []
+  path = path.rstrip(os.sep)
+  for fname in os.listdir(path):
+    images.append((open(os.path.join(path, fname), 'rb'), fname))
+  return api.tag_images(images)
+
+
+def main(argv):
+  if len(argv) > 1:
+    imageurl = argv[1]
+  else:
+    imageurl = 'http://clarifai-img.s3.amazonaws.com/test/toddler-flowers.jpeg'
+
+  api = ClarifaiApi()
+  #api.set_model("")
+  if imageurl.startswith('http'):
+    response = api.tag_image_urls(imageurl)
+  elif os.path.isdir(imageurl):
+    response = tag_images_in_directory(imageurl)
+  elif os.path.isfile(imageurl):
+    with open(imageurl,'rb') as image_file:
+      response = api.tag(image_file, select_classes="pretty")
+  else:
+    raise Exception("Must input url, directory path, or file path")
+
+  resultCode = json.dumps(response)
+  print resultCode
+
+
+if __name__ == '__main__':
+  main(sys.argv)
